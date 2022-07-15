b353b5f6091228116e22b1a8ea2b4bf1a456aa98
Raphael Bovi <93729728+RaphaelBovi@users.noreply.github.com>
2022-07-15 12:11:46 -0300

    first commit

---
+++ src/test/java/com/taxa/selic/SelicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxa.selic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
